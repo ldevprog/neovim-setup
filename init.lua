087b39268ce09b9df39016424dbfac922b00fbef
Levon Dalakyan <wman8800@gmail.com>
2024-10-05 11:31:22 +0300

    improvements

--- init.lua
+++ init.lua
@@ -9,13 +9,22 @@ if not vim.loop.fs_stat(lazypath) then
     vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
+vim.g.translate_source = "ru"
+vim.g.translate_target = "en"
+vim.g.NERDTreeShowHidden = 1
+
 vim.opt.rtp:prepend(lazypath)
 
 vim.opt.number = true
 
--- vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 
-vim.g.NERDTreeShowHidden = 1
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
 
 local lazy_config = require "configs.lazy"
 
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "luals", "html", "cssls", "tsserver", "clangd", "gopls", "gradle_ls", "vuels" }
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "gradle_ls", "vuels" }
 
 lspconfig.servers = servers
 
