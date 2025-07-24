8744874b705d752643577f7784e28fbd90734d77
levon-dalakyan <wman8800@gmail.com>
2025-07-24 21:26:17 +0300

    add smooth scroll; add surrounding

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -204,21 +204,46 @@ return {
         opts = {
             -- Smear cursor when switching buffers or windows.
             smear_between_buffers = true,
-
-            -- Smear cursor when moving within line or to neighbor lines.
-            -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
             smear_between_neighbor_lines = true,
-
-            -- Draw the smear in buffer space instead of screen space when scrolling
             scroll_buffer_space = true,
-
-            -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-            -- Smears will blend better on all backgrounds.
             legacy_computing_symbols_support = false,
 
-            -- Smear cursor in insert mode.
-            -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+            -- Faster smear
             smear_insert_mode = true,
+            stiffness = 0.8, -- 0.6      [0, 1]
+            trailing_stiffness = 0.5, -- 0.4      [0, 1]
+            stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+            trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+            damping = 0.8, -- 0.65     [0, 1]
+            damping_insert_mode = 0.8, -- 0.7      [0, 1]
+            distance_stop_animating = 0.5, -- 0.1      > 0
+            time_interval = 10, -- milliseconds
+
+            -- cursor_color = "#ff8800",
+            -- stiffness = 0.3,
+            -- trailing_stiffness = 0.1,
+            -- damping = 0.5,
+            -- trailing_exponent = 5,
+            -- never_draw_over_target = true,
+            -- hide_target_hack = true,
+            -- gamma = 1,
+        },
+    },
+    {
+        "karb94/neoscroll.nvim",
+        lazy = false,
+        opts = {
+            duration_multiplier = 0.5,
+        },
+    },
+    {
+        "echasnovski/mini.surround",
+        lazy = false,
+        version = "*",
+        opts = {
+            mappings = {
+                add = "sa",
+            },
         },
     },
 }
