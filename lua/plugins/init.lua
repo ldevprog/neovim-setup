011c9c64f7e481d4ebd73712392f6422063c1308
ldevprog <wman8800@gmail.com>
2025-08-31 12:44:05 +0300

    fix

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -328,9 +328,6 @@ return {
         ---@type Flash.Config
         opts = {
             modes = {
-                search = {
-                    enabled = true,
-                },
                 char = {
                     jump_labels = true,
                 },
